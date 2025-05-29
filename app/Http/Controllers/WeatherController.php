4cd627828247bd1464d6f7e08b2778bdfe165977
Igor Carvalho Braz <igoorcvb@MacBook-Air-de-Igor.local>
2025-05-29 16:09:21 -0300

    feat: adding controller, service and routes

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Http;
+
+class WeatherController extends Controller
+{
+    public function getWeather($city)
+    {
+        try {
+            $response = Http::get('https://api.openweathermap.org/data/2.5/weather', [
+                'q' => $city,
+                'appid' => config('services.openweathermap.key'),
+                'units' => 'metric',
+                'lang' => 'pt_br',
+            ]);
+
+
+            if ($response->failed()) {
+                return response()->json([
+                    'error' => 'Erro ao buscar dados da API',
+                    'message' => $response->json(), // Aqui mostra o erro exato da API
+                ], $response->status());
+            }
+
+            return response()->json($response->json());
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Erro interno',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+    
+}
